f7819673b421b7e32844d98cdf3ceac79feb5f72
ZEER0H <calbers1@harding.edu>
2020-12-11 15:41:44 -0600

    added fade-in and fade-out on timer for the message display, with style binding.

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -9,17 +9,25 @@ export class MenuComponent implements OnInit {
 
   constructor() { }
 
-
   messages = ["", "This", "is", "harder", "than", "it", "seems"];
   textCounter = 0;
   msg = this.messages[this.textCounter]
+  transition = '0s';
+  opacity = '0%';
   changeText(){
+    this.transition = '0.5s';
+    this.opacity = '0%';
     this.textCounter++;
-    this.msg = this.messages[this.textCounter]
-    
-    if (this.textCounter >= 7){
+    if (this.textCounter > 6){
       this.textCounter = 0;
     }
+    
+    setTimeout(() => {
+      this.msg = this.messages[this.textCounter];
+      this.opacity = '100%';
+      this.transition = '0.5s'
+    }
+    , 500);
   }
 
   ngOnInit(): void {
